d71c0992e5873011dde8c560ad14a499d1e6fa42
Heiki Riesenkampf <hr1514@ic.ac.uk>
2018-04-09 10:36:34 +0200

    Change back to 1 GPU

--- CT-GANs/tensorflow_generative_model/CT_gan_cifar_resnet.py
+++ CT-GANs/tensorflow_generative_model/CT_gan_cifar_resnet.py
@@ -31,7 +31,7 @@ DATA_DIR = '../../../data'    #file path to be modified
 if len(DATA_DIR) == 0:
     raise Exception('Please specify path to data directory in gan_cifar.py!')
 
-N_GPUS = 2
+N_GPUS = 1
 if N_GPUS not in [1,2]:
     raise Exception('Only 1 or 2 GPUs supported!')
 
